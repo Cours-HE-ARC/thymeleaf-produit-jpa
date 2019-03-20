c687cb25023a756446f61ef7ba0d6233541c3636
Sébastien Chèvre <seb.chevre@gmail.com>
2019-03-20 06:56:53 +0100

    initial commit

---
+++ src/test/java/com/example/demo/ProduitTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+class ProduitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
